14bd1be5036c85a9f1db8e86f7942743332646ea
Muntazir Abbas <muntazirabbas24@gmail.com>
2020-03-07 17:45:48 +0500

    linking set

--- app.js
+++ app.js
@@ -1,52 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mustacheExprees = require('mustache-express');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mustacheExprees = require('mustache-express');
 
-var indexRouter = require('./routes/generic/index');
-var packageRouter = require('./routes/generic/all-package');
-var toursRouter = require('./routes/generic/tours');
-var toursDetailRouter = require('./routes/generic/tourdetail');
-var blogRouter = require('./routes/generic/blog');
-var blogDetailRouter = require('./routes/generic/blogdetail');
-var aboutRouter = require('./routes/generic/about');
-var contactRouter = require('./routes/generic/contact');
-var siteMapRouter = require('./routes/generic/siteMap');
+const indexRouter = require('./routes/generic/index');
+const notFoundRouter = require('./routes/generic/404');
+const packageRouter = require('./routes/generic/all-package');
+const toursRouter = require('./routes/generic/tours');
+const toursDetailRouter = require('./routes/generic/tourdetail');
+const blogRouter = require('./routes/generic/blog');
+const blogDetailRouter = require('./routes/generic/blogdetail');
+const aboutRouter = require('./routes/generic/about');
+const contactRouter = require('./routes/generic/contact');
+const siteMapRouter = require('./routes/generic/siteMap');
 
-var userLoginRouter = require('./routes/generic/userLogin');
-var userProfileRouter = require('./routes/generic/userProfile');
-var userDashboardRouter = require('./routes/generic/userDashboard');
-var travelBookingRouter = require('./routes/generic/travelBooking');
-var travelDetailRouter = require('./routes/generic/travelDetail');
+const userLoginRouter = require('./routes/generic/userLogin');
+const userProfileRouter = require('./routes/generic/userProfile');
+const userDashboardRouter = require('./routes/generic/userDashboard');
+const travelBookingRouter = require('./routes/generic/travelBooking');
+const travelDetailRouter = require('./routes/generic/travelDetail');
 
 
 //adminRouting
-var addBlogRouter = require('./routes/admin/addBlog');
-var addPackageRouter = require('./routes/admin/addPackage');
-var addPackageCategoryRouter = require('./routes/admin/addPackageCategory');
-var addSightSeeingRouter = require('./routes/admin/addSightSeeing');
-var allBlogsRouter = require('./routes/admin/allBlogs');
-var allPackageBookingsRouter = require('./routes/admin/allPackageBookings');
-var allPackageCategoriesRouter = require('./routes/admin/allPackageCategories');
-var allUsersRouter = require('./routes/admin/allUsers');
-var dashboardRouter = require('./routes/admin/dashboard');
-var loginRouter = require('./routes/admin/login');
-var sightSeeBookingsRouter = require('./routes/admin/sightSeeBookings');
-var sightSeeingRouter = require('./routes/admin/sightSeeing');
-var socialMediaRouter = require('./routes/admin/socialMedia');
-var tourPackagesRouter = require('./routes/admin/tourPackages');
+const addBlogRouter = require('./routes/admin/addBlog');
+const addPackageRouter = require('./routes/admin/addPackage');
+const addPackageCategoryRouter = require('./routes/admin/addPackageCategory');
+const addSightSeeingRouter = require('./routes/admin/addSightSeeing');
+const allBlogsRouter = require('./routes/admin/allBlogs');
+const allPackageBookingsRouter = require('./routes/admin/allPackageBookings');
+const allPackageCategoriesRouter = require('./routes/admin/allPackageCategories');
+const allUsersRouter = require('./routes/admin/allUsers');
+const dashboardRouter = require('./routes/admin/dashboard');
+const loginRouter = require('./routes/admin/login');
+const forgetRouter = require('./routes/admin/forgot');
+const sightSeeBookingsRouter = require('./routes/admin/sightSeeBookings');
+const sightSeeingRouter = require('./routes/admin/sightSeeing');
+const socialMediaRouter = require('./routes/admin/socialMedia');
+const tourPackagesRouter = require('./routes/admin/tourPackages');
 
 
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.engine('mustache', mustacheExprees());
-// app.set('views', path.join(__dirname, '/views/admin'))
-// app.set('views', path.join(__dirname, '/views/generic'));
-// app.set('views', [__dirname + '/views/admin', __dirname + '/views/generic']);
 app.set('views', __dirname + '/views');
 
 
@@ -59,18 +57,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-app.use('/', indexRouter);
 
-app.use('/allPackages', packageRouter);
+// setting routes
+app.use('/', indexRouter);
+app.use('/404', indexRouter);
+app.use('/main', indexRouter); //index router
+app.use('/all-package', packageRouter);
 app.use('/tours', toursRouter);
-app.use('/tourDetail', toursDetailRouter);
+app.use('/tour-details', toursDetailRouter);
 app.use('/blog', blogRouter);
-app.use('/blogDetail', blogDetailRouter);
+app.use('/blog-details', blogDetailRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
 app.use('/siteMap', siteMapRouter);
-
 app.use('/login', userLoginRouter);
 app.use('/profile', userProfileRouter);
 app.use('/dashboard', userDashboardRouter);
@@ -78,24 +77,24 @@ app.use('/travelBooking', travelBookingRouter);
 app.use('/travelDetail', travelDetailRouter);
 
 
-//admin routes
+// admin routes
 
 app.use('/admin', dashboardRouter);
-app.use('/admin/addBlog', addBlogRouter);
-app.use('/admin/addPackage', addPackageRouter);
-app.use('/admin/addPackageCategory', addPackageCategoryRouter);
-app.use('/admin/addSightSeeing', addSightSeeingRouter);
-app.use('/admin/allUsers', allUsersRouter);
-app.use('/admin/dashboard', dashboardRouter);
+app.use('/admin/blog-add', addBlogRouter);
+app.use('/admin/package-add', addPackageRouter);
+app.use('/admin/package-cat-add', addPackageCategoryRouter);
+app.use('/admin/sight-see-add', addSightSeeingRouter);
+app.use('/admin/user-all', allUsersRouter);
+app.use('/admin/index', dashboardRouter);
 app.use('/admin/login', loginRouter);
-app.use('/admin/sightSeeBookings', sightSeeBookingsRouter);
-app.use('/admin/sightSeeing', sightSeeingRouter);
-app.use('/admin/socialMedia', socialMediaRouter);
-app.use('/admin/tourPackages', tourPackagesRouter);
-app.use('/admin/allBlogs', allBlogsRouter);
-app.use('/admin/allPackageCategories', allPackageCategoriesRouter);
-app.use('/admin/allPackageBookings', allPackageBookingsRouter);
-
+app.use('/admin/forgot', forgetRouter);
+app.use('/admin/social-media', socialMediaRouter);
+app.use('/admin/package-all', tourPackagesRouter);
+app.use('/admin/blog-all', allBlogsRouter);
+app.use('/admin/package-cat-all', allPackageCategoriesRouter);
+app.use('/admin/package-booking-all', allPackageBookingsRouter);
+app.use('/admin/sight-see-add', sightSeeBookingsRouter);
+app.use('/admin/sight-see-all', sightSeeingRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/admin/forgot.js
+++ routes/admin/forgot.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('admin/addPackageCategory', { title: 'Desert Safari' });
+    res.render('admin/forgot', { title: 'Desert Safari' });
 });
 
 module.exports = router;
